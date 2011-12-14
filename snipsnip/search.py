681166631328d9a75793d9210861db48c391b694
Joseph McCullough <joseph@vertstudios.com>
2011-12-14 00:01:50 -0600

    Test of writing snippet

--- snipsnip/search.py
+++ snipsnip/search.py
@@ -12,26 +12,26 @@ class Searcher:
 	def __init__(self, indexdir):
 		# Open the index if it exists
 		try:
-			ix = open_dir(indexdir)
+			self.ix = open_dir(indexdir)
 			print "FOUND"
 		except EmptyIndexError:
-			ix = create_in(indexdir, schema)
-		writer = ix.writer()
+			self.ix = create_in(indexdir, schema)
+		self.writer = self.ix.writer()
 
 	def add_snippet_to_index(self, description, lang):
 		fileName = file_name_from_string(description)
-		writer.add_document(
+		self.writer.add_document(
 				description = unicode(description),
 				path = unicode(fileName),
 				lang = unicode(lang)
 		)
 		text_editor(fileName)
-		writer.commit()
+		self.writer.commit()
 	
 	@staticmethod
 	def search(self, searchStr, lang):
-		with ix.searcher() as searcher:
-			qp = QueryParser("description", ix.schema, group=OrGroup)
+		with self.ix.searcher() as searcher:
+			qp = QueryParser("description", self.ix.schema, group=OrGroup)
 			query = qp.parse(unicode("(%s) AND (lang:%s)" % (searchStr, lang)))
 			results = searcher.search(query)
 		return results
