cd740b85c4654c1fae14b3c0947fc03b391b787c
Joseph McCullough <joseph@vertstudios.com>
2012-04-08 02:40:34 -0500

    Changed tabs to spaces, ahhh. Closes #21

--- snip/curseshelpers.py
+++ snip/curseshelpers.py
@@ -3,111 +3,111 @@ import curses, os, re, subprocess, tempfile, sys, time
 from globals import *
 
 class WindowTemplate:
-	'''
-	A simple template to pass down default specifications for new windows
-	'''
+  '''
+  A simple template to pass down default specifications for new windows
+  '''
 
-	def __init__(self, top=0, left=0, width=0, height=0):
-		self.top = top
-		self.left = left
-		self.width = width
-		self.height = height
+  def __init__(self, top=0, left=0, width=0, height=0):
+    self.top = top
+    self.left = left
+    self.width = width
+    self.height = height
 
-	def new(self, top=0, left=0, width=0, height=0):
-		'''
-		Create a new window just like cursed, but default values are passed
-		down from initialization
-		'''
-		top = top or self.top
-		left = left or self.left
-		width = width or self.width
-		height = height or self.height
-		return curses.newwin(height, width, top, left)
+  def new(self, top=0, left=0, width=0, height=0):
+    '''
+    Create a new window just like cursed, but default values are passed
+    down from initialization
+    '''
+    top = top or self.top
+    left = left or self.left
+    width = width or self.width
+    height = height or self.height
+    return curses.newwin(height, width, top, left)
 
 
 class Window:
-	'''
-	Add convienence methods for a curses window
-	'''
+  '''
+  Add convienence methods for a curses window
+  '''
 
-	def __init__(self, template=None, top=0, left=0, width=0, height=0):
-		# If a template was provided, use it.
-		if template:
-			self.win = template.new(top, left, width, height)
-		else:
-			self.win = curses.newwin(height, width, top, left)
+  def __init__(self, template=None, top=0, left=0, width=0, height=0):
+    # If a template was provided, use it.
+    if template:
+      self.win = template.new(top, left, width, height)
+    else:
+      self.win = curses.newwin(height, width, top, left)
 
-		# Create some wrappers for commonly used methods
-		self.write = self.win.addstr
-		self.draw = self.win.refresh
+    # Create some wrappers for commonly used methods
+    self.write = self.win.addstr
+    self.draw = self.win.refresh
 
-		# Allow arrow keys
-		self.win.keypad(1)
+    # Allow arrow keys
+    self.win.keypad(1)
 
-	def println(self, s):
-		self.win.addstr(s + "\n")
+  def println(self, s):
+    self.win.addstr(s + "\n")
 
-	def flash(self,s, x=None,y=None):
-		'''
-		Draw string s and immediately print it at (x,y)
-		'''
+  def flash(self,s, x=None,y=None):
+    '''
+    Draw string s and immediately print it at (x,y)
+    '''
 
-		# If x and y not specified, just write from cursor.
-		if x is None and y is None:
-			self.win.addstr(s)
-		else:
-			self.win.addstr(x,y,s)
-		self.win.refresh()
+    # If x and y not specified, just write from cursor.
+    if x is None and y is None:
+      self.win.addstr(s)
+    else:
+      self.win.addstr(x,y,s)
+    self.win.refresh()
 
-	def test(self, testString="Testing\n"):
-		'''
-		Simple display of the window with a border around it for reference
-		'''
-		self.win.border()
-		self.win.addstr(testString + "\n")
-		self.win.refresh()
+  def test(self, testString="Testing\n"):
+    '''
+    Simple display of the window with a border around it for reference
+    '''
+    self.win.border()
+    self.win.addstr(testString + "\n")
+    self.win.refresh()
 
-	def clear(self):
-		'''
-		Clear and refresh window
-		'''
-		self.win.clear()
-		self.win.refresh()
+  def clear(self):
+    '''
+    Clear and refresh window
+    '''
+    self.win.clear()
+    self.win.refresh()
 
-	def read(self):
-		'''
-		Read a string, execute necessary configurations
-		'''
+  def read(self):
+    '''
+    Read a string, execute necessary configurations
+    '''
 
-		# Turn on echo of input text and blinking cursor.
-		curses.curs_set(1)  
-		curses.echo()
-		text = self.win.getstr()
-		curses.noecho()
-		curses.curs_set(0)  
-		return text
+    # Turn on echo of input text and blinking cursor.
+    curses.curs_set(1)  
+    curses.echo()
+    text = self.win.getstr()
+    curses.noecho()
+    curses.curs_set(0)  
+    return text
 
-	
+  
 
 def text_editor(fileName):
-	'''
-	Launch the text editor represented by the EDITOR environment
-	variable
-	From stack overflow (http://bit.ly/tBzqgz)
-	'''
-	EDITOR = os.environ.get('EDITOR','vim') 
-	fullPath = os.path.join(SNIPPETS_DIR, fileName)
+  '''
+  Launch the text editor represented by the EDITOR environment
+  variable
+  From stack overflow (http://bit.ly/tBzqgz)
+  '''
+  EDITOR = os.environ.get('EDITOR','vim') 
+  fullPath = os.path.join(SNIPPETS_DIR, fileName)
 
-	# Try to open the file for updating if it exists. If it doesn't exist,
-	# open the file in write mode.
-	content = ""
-	try:
-		f = open(fullPath, 'r')
-	except:
-		f = open(fullPath, 'w')
+  # Try to open the file for updating if it exists. If it doesn't exist,
+  # open the file in write mode.
+  content = ""
+  try:
+    f = open(fullPath, 'r')
+  except:
+    f = open(fullPath, 'w')
 
-	# Have to init and endwin to prevent cursor from sticking around
-	curses.initscr()
-	proc = subprocess.call([EDITOR, f.name])
-	curses.endwin()
-	f.close()
+  # Have to init and endwin to prevent cursor from sticking around
+  curses.initscr()
+  proc = subprocess.call([EDITOR, f.name])
+  curses.endwin()
+  f.close()
--- snip/search.py
+++ snip/search.py
@@ -14,169 +14,169 @@ schema = Schema(description=TEXT(stored=True), path=ID(stored=True), lang=TEXT(a
 from curseshelpers import *
 
 class Searcher:
-	'''
-	Our search engine class! I, for one, welcome our Googly overlords.
-	'''
+  '''
+  Our search engine class! I, for one, welcome our Googly overlords.
+  '''
 
-	def __init__(self, indexdir):
-		# Open the index if it exists
-		try:
-			self.ix = open_dir(indexdir)
-		except EmptyIndexError:
-			self.ix = create_in(indexdir, schema)
-		self.indexdir = indexdir
+  def __init__(self, indexdir):
+    # Open the index if it exists
+    try:
+      self.ix = open_dir(indexdir)
+    except EmptyIndexError:
+      self.ix = create_in(indexdir, schema)
+    self.indexdir = indexdir
 
-	def get_writer(self):
-		self.writer = self.ix.writer()
+  def get_writer(self):
+    self.writer = self.ix.writer()
 
-	def add_snippet_to_index(self, description, lang):
-		''' Add a new code snippet to the index.
-		description: description of the index
-		lang: the language/framework.
-		
-		s.add_snippet_to_index("Append to list", "Python")
-		'''
+  def add_snippet_to_index(self, description, lang):
+    ''' Add a new code snippet to the index.
+    description: description of the index
+    lang: the language/framework.
+    
+    s.add_snippet_to_index("Append to list", "Python")
+    '''
 
-		self.get_writer()
+    self.get_writer()
 
-		# To prevent a pluralization from causing the searches to fail,
-		# store the singular version of the description string for indexing
-		# and the raw sting for presentation.
+    # To prevent a pluralization from causing the searches to fail,
+    # store the singular version of the description string for indexing
+    # and the raw sting for presentation.
 
-		# pathName looks like so: python/how_to_use_lists_12342342
-		# fileName is the absolute path to the pathName on the system.
-		fileName = snippet_file_name(description)
-		pathName = os.path.join(lang_dir(lang, False), fileName)
+    # pathName looks like so: python/how_to_use_lists_12342342
+    # fileName is the absolute path to the pathName on the system.
+    fileName = snippet_file_name(description)
+    pathName = os.path.join(lang_dir(lang, False), fileName)
 
-		self.writer.add_document(
-				description = unicode(self.singularize(description).lower()),
-				_stored_description = unicode(description),
-				path = unicode(pathName),
-				lang = unicode(lang)
-		)
+    self.writer.add_document(
+        description = unicode(self.singularize(description).lower()),
+        _stored_description = unicode(description),
+        path = unicode(pathName),
+        lang = unicode(lang)
+    )
 
-		fileName = os.path.join(lang_dir(lang), fileName)
-		text_editor(fileName)
-		self.writer.commit()
-	
-	def add_file_to_index(self, path, description):
-		'''
-		Add a snippet that already exists on the file system to the index
-		'''
-		self.get_writer()
-		lang = path.split('/')[0]
+    fileName = os.path.join(lang_dir(lang), fileName)
+    text_editor(fileName)
+    self.writer.commit()
+  
+  def add_file_to_index(self, path, description):
+    '''
+    Add a snippet that already exists on the file system to the index
+    '''
+    self.get_writer()
+    lang = path.split('/')[0]
 
-		self.writer.add_document(
-				description = unicode(self.singularize(description).lower()),
-				_stored_description = unicode(description),
-				path = unicode(path),
-				lang = unicode(lang)
-		)
+    self.writer.add_document(
+        description = unicode(self.singularize(description).lower()),
+        _stored_description = unicode(description),
+        path = unicode(path),
+        lang = unicode(lang)
+    )
 
-		self.writer.commit()
-	
-	def delete(self, path):
-		'''
-		Delete a snippet from the index and the file system.
-		path is the lang/snippetDescription path of the snippet
-		'''
-		
-		lang = path.split('/')[0]
-		snippet = path.split('/')[1]
-		self.get_writer()
+    self.writer.commit()
+  
+  def delete(self, path):
+    '''
+    Delete a snippet from the index and the file system.
+    path is the lang/snippetDescription path of the snippet
+    '''
+    
+    lang = path.split('/')[0]
+    snippet = path.split('/')[1]
+    self.get_writer()
 
-		# Delete based on the path, since it is unique.
-		self.writer.delete_by_term('path', path)
-		self.writer.commit()
+    # Delete based on the path, since it is unique.
+    self.writer.delete_by_term('path', path)
+    self.writer.commit()
 
-		# Now that we've deleted the snippet from the index, we delete
-		# it from the file system.
+    # Now that we've deleted the snippet from the index, we delete
+    # it from the file system.
 
-		# Get the full path of the snippet file.
-		snipLoc = os.path.join(lang_dir(lang), snippet)
+    # Get the full path of the snippet file.
+    snipLoc = os.path.join(lang_dir(lang), snippet)
 
-		# Now we delete it! We're done!
-		os.remove(snipLoc)
-	
-	def rename(self, path, newDescription):
-		'''
-		Update a snippet based on the path. Add a new description via newDescription
-		'''
+    # Now we delete it! We're done!
+    os.remove(snipLoc)
+  
+  def rename(self, path, newDescription):
+    '''
+    Update a snippet based on the path. Add a new description via newDescription
+    '''
 
-		# 1. Store the contents of the snippet file in path 
-		# 2. Get filename from newDescription and copy contents 
-		# 3. Delete the snippet and remove from index
-		# 4. Create new snippet
+    # 1. Store the contents of the snippet file in path 
+    # 2. Get filename from newDescription and copy contents 
+    # 3. Delete the snippet and remove from index
+    # 4. Create new snippet
 
-		# 1. Store the contents of the old snippet file 
-		lang = path.split('/')[0]
-		oldSnippetFileName = path.split('/')[1]
-		oldSnippetLoc = os.path.join(lang_dir(lang), oldSnippetFileName)
-		f = open(oldSnippetLoc, 'r')
-		snippetContent = f.read()
-		f.close()
+    # 1. Store the contents of the old snippet file 
+    lang = path.split('/')[0]
+    oldSnippetFileName = path.split('/')[1]
+    oldSnippetLoc = os.path.join(lang_dir(lang), oldSnippetFileName)
+    f = open(oldSnippetLoc, 'r')
+    snippetContent = f.read()
+    f.close()
 
-		# 2. Get filename from newDescription and copy contents 
-		newFileName = snippet_file_name(newDescription)
-		newLoc = os.path.join(lang_dir(lang), newFileName)
-		f = open(newLoc, 'w')
-		f.write(snippetContent)
-		f.close()
+    # 2. Get filename from newDescription and copy contents 
+    newFileName = snippet_file_name(newDescription)
+    newLoc = os.path.join(lang_dir(lang), newFileName)
+    f = open(newLoc, 'w')
+    f.write(snippetContent)
+    f.close()
 
-		# 3. Delete the snippet and remove from index
-		self.delete(path)
+    # 3. Delete the snippet and remove from index
+    self.delete(path)
 
-		# 4. Create new snippet
-		newPath = os.path.join(lang_dir(lang, False), newFileName)
-		self.add_file_to_index(newPath, newDescription)
+    # 4. Create new snippet
+    newPath = os.path.join(lang_dir(lang, False), newFileName)
+    self.add_file_to_index(newPath, newDescription)
 
 
-	
-	def search(self, searchStr, lang):
-		''' Search for a code snippet based off a search string (searchStr)
-		and the language
+  
+  def search(self, searchStr, lang):
+    ''' Search for a code snippet based off a search string (searchStr)
+    and the language
 
-		s.search("How to append to a list", "Python")
-		'''
-		with self.ix.searcher() as searcher:
-			qp = QueryParser("description", self.ix.schema, group=OrGroup)
+    s.search("How to append to a list", "Python")
+    '''
+    with self.ix.searcher() as searcher:
+      qp = QueryParser("description", self.ix.schema, group=OrGroup)
 
-			# Since the index is singularized, we must search using a
-			# singularized string.
-			query = qp.parse(unicode("(%s) AND (lang:%s)" % (self.singularize(searchStr), lang)))
-			results = searcher.search(query)
-			returnThis = [ (x['description'].lower(), x['path']) for x in results]
-			return returnThis
+      # Since the index is singularized, we must search using a
+      # singularized string.
+      query = qp.parse(unicode("(%s) AND (lang:%s)" % (self.singularize(searchStr), lang)))
+      results = searcher.search(query)
+      returnThis = [ (x['description'].lower(), x['path']) for x in results]
+      return returnThis
 
-	def get_lang(self, lang):
-		'''
-		Get all results matching the language
-		'''
-		with self.ix.searcher() as searcher:
-			qp = QueryParser("lang", self.ix.schema)
-			query = qp.parse(unicode(lang))
-			results = searcher.search(query, sortedby="description", limit=None)
-			returnThis = [ (x['description'].lower(), x['path']) for x in results]
+  def get_lang(self, lang):
+    '''
+    Get all results matching the language
+    '''
+    with self.ix.searcher() as searcher:
+      qp = QueryParser("lang", self.ix.schema)
+      query = qp.parse(unicode(lang))
+      results = searcher.search(query, sortedby="description", limit=None)
+      returnThis = [ (x['description'].lower(), x['path']) for x in results]
 
-			# Sort by alphabet, case insensitive
-			returnThis.sort(key=lambda x:x[0].lower())
-			return returnThis
-	
-	def singularize(self, theStr):
-		'''
-		Return a singularized string. For example,
-		"Rails routes variables" => "Rail route variable"
-		'''
-		p = inflect.engine()
-		normalList = [x for x in theStr.split(' ')]
-		singularList = [p.singular_noun(x) for x in normalList]
+      # Sort by alphabet, case insensitive
+      returnThis.sort(key=lambda x:x[0].lower())
+      return returnThis
+  
+  def singularize(self, theStr):
+    '''
+    Return a singularized string. For example,
+    "Rails routes variables" => "Rail route variable"
+    '''
+    p = inflect.engine()
+    normalList = [x for x in theStr.split(' ')]
+    singularList = [p.singular_noun(x) for x in normalList]
 
-		# p.sinuglar_noun returns False if the word is already singular,
-		# so just grab the word from normalList.
-		returnList = [x or y for (x,y) in zip(singularList, normalList)]
+    # p.sinuglar_noun returns False if the word is already singular,
+    # so just grab the word from normalList.
+    returnList = [x or y for (x,y) in zip(singularList, normalList)]
 
-		# Now join back into a string
-		return " ".join(returnList)
+    # Now join back into a string
+    return " ".join(returnList)
 
 
 ########################################################
@@ -184,42 +184,42 @@ class Searcher:
 ########################################################
 
 def file_name_safe(theStr, joinChar='_'):
-	'''
-	Return a file name safe string. Extracts groups of numbers
-	and letters from the string and joins with joinChar. Also
-	lower cases the string.
-	'''
-	tmpList = re.findall(r'[a-zA-Z0-9]+', theStr)
-	return joinChar.join([x.lower() for x in tmpList])
+  '''
+  Return a file name safe string. Extracts groups of numbers
+  and letters from the string and joins with joinChar. Also
+  lower cases the string.
+  '''
+  tmpList = re.findall(r'[a-zA-Z0-9]+', theStr)
+  return joinChar.join([x.lower() for x in tmpList])
 
 
 def snippet_file_name(description):
-	'''
-	Get a file safe name for the description with a timestamp 
-	attached to avoid collisions.
-	'''
-	joinChar = '_'
-	description = file_name_safe(description, joinChar)
+  '''
+  Get a file safe name for the description with a timestamp 
+  attached to avoid collisions.
+  '''
+  joinChar = '_'
+  description = file_name_safe(description, joinChar)
 
-	# Join with underscores and make lowercase.
-	return joinChar.join([description, str( int(time.time()) )])
+  # Join with underscores and make lowercase.
+  return joinChar.join([description, str( int(time.time()) )])
 
 def lang_dir(lang, full=True):
-	'''
-	Get a string representing the directory of the language
-	full: If full is True, get the absolute path to the file.
-	'''
-	if full:
-		return os.path.join(SNIPPETS_DIR, lang.lower().strip())
-	else:
-		return lang.lower().strip()
+  '''
+  Get a string representing the directory of the language
+  full: If full is True, get the absolute path to the file.
+  '''
+  if full:
+    return os.path.join(SNIPPETS_DIR, lang.lower().strip())
+  else:
+    return lang.lower().strip()
 
 
 def quick_read(fileName):
-	f = open(fileName, 'r')
-	items = sorted([x.strip() for x in f.readlines()], key=str.lower)
-	f.close()
-	return items
+  f = open(fileName, 'r')
+  items = sorted([x.strip() for x in f.readlines()], key=str.lower)
+  f.close()
+  return items
 
 GoogleBot = Searcher(WHOOSH_INDEX)
 #result = GoogleBot.search("Hello World", "c++")
